6a4b116e15bb83f4af8a7f82ff1eb2907d5be1f3
DLon <danlonngren@gmail.com>
2025-07-13 14:24:22 +0100

    Refactor and cleanup code

--- simplePIDController/sPIDController.cpp
+++ simplePIDController/sPIDController.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-sPIDController::sPIDController(float kp, float ki, float kd, float outMax) :
+SPIDController::SPIDController(float kp, float ki, float kd, float outMax) :
     m_pidData(), 
     m_kp(kp), 
     m_ki(ki), 
@@ -12,60 +12,56 @@ sPIDController::sPIDController(float kp, float ki, float kd, float outMax) :
     m_maxOutput(outMax), 
     m_pidOutput(0.0f) {}
 
-float sPIDController::evaluate(float input, float setpoint, float dt) {
-    // Avoid division by zero
-    if (dt <= 0.0f) 
-        return m_pidOutput;
-  
-    m_pidData.pError = setpoint - input;
+float SPIDController::evaluate(float input, float setpoint, float dt) {
+    float error = setpoint - input;
     
+    // Avoid division by zero
+    if (dt <= 0.0f) return m_pidOutput;
+  
     // If we just started, initialize the last error
     // This prevents a large spike in the first output
     if (!m_started) {
         m_started = true;
-        m_pidData.eLast = m_pidData.pError;
+        m_pidData.eLast = error;
     }
-
-    m_pidData.dError = (m_pidData.pError - m_pidData.eLast) / dt;
-    m_pidData.eLast = m_pidData.pError;
-    
-    // Integral error with anti-windup
-    m_pidData.iError += m_pidData.pError * dt;
-    if (m_pidData.iError > m_maxOutput)
-        m_pidData.iError = m_maxOutput;
-    else if (m_pidData.iError < -m_maxOutput)
-        m_pidData.iError = -m_maxOutput;
+            
+    m_pidData.pError  = error;
+    m_pidData.iError += error * dt;
+    m_pidData.dError  = (error - m_pidData.eLast) / dt;
+    m_pidData.eLast   = error;
     
     // PID output calculation
-	m_pidOutput = m_pidData.pError * m_kp + m_pidData.iError * m_ki + m_pidData.dError * m_kd;
+	m_pidOutput = (m_pidData.pError * m_kp) + 
+                  (m_pidData.iError * m_ki) + 
+                  (m_pidData.dError * m_kd);
+
     m_pidOutput = limiter(m_pidOutput, (-m_maxOutput), m_maxOutput);
-        
 	return m_pidOutput;
 }
 
-void sPIDController::reset() {
+void SPIDController::reset() {
 	m_pidData.clear();
     m_pidOutput = 0.0f;
     m_started = false;
 }
 
-void sPIDController::setPidGains(float kp, float ki, float kd) {
+void SPIDController::setPidGains(float kp, float ki, float kd) {
 	m_kp = kp;
 	m_ki = ki;
 	m_kd = kd;
 }
 
-void sPIDController::setOutputMax(float outMax) {
+void SPIDController::setOutputMax(float outMax) {
 	m_maxOutput = outMax;
 }
 
-float sPIDController::limiter(float value, float min, float max) {
+float SPIDController::limiter(float value, float min, float max) {
     if (value > max) return max;
     if (value < min) return min;
     return value;
 }
 
-void sPIDController::print() const {
+void SPIDController::print() const {
     std::cout << "PID Output        : " << m_pidOutput << "\n"
               << "Error             : " << m_pidData.pError << "\n"
               << "Integral Error    : " << m_pidData.iError << "\n"
--- simplePIDController/sPIDController.h
+++ simplePIDController/sPIDController.h
@@ -1,19 +1,30 @@
 #pragma once
 
-class sPIDController {
+struct PidState {
+    float pError = 0.0f;
+    float iError = 0.0f;
+    float dError = 0.0f;
+    float eLast  = 0.0f;
+
+    void clear() {
+        pError = iError = dError = eLast = 0.0f;
+    }
+};
+
+class IsPIDController {
+public:
+    virtual ~IsPIDController() = default;
+    virtual float evaluate(float input, float setpoint, float dt) = 0;
+    virtual void reset() = 0;
+    virtual void setPidGains(float kp, float ki, float kd) = 0;
+    virtual void setOutputMax(float outMax) = 0;
+    virtual float getPidLastOutput() const = 0;
+    virtual const struct PidState& getState() const = 0;
+};
+
+class SPIDController : public IsPIDController {
 private:
-    struct pidData {
-        pidData() : pError(0.0f), iError(0.0f), dError(0.0f), eLast(0.0f) {}
-        ~pidData() = default;
-        
-        float pError, iError, dError, eLast;
-
-        void clear() {
-            pError = iError = dError = eLast = 0.0f;
-        }
-    };
-
-    pidData m_pidData;
+    PidState m_pidData;
     float m_kp, m_ki, m_kd;
     float m_pidOutput;
     float m_maxOutput;
@@ -21,14 +32,18 @@ private:
     bool m_started = false;
 
 public:
-    sPIDController(float kp, float ki, float kd, float outMax);
-    ~sPIDController() = default;
+    SPIDController(float kp, float ki, float kd, float outMax);
+    ~SPIDController() = default;
 
     float evaluate(float input, float setpoint, float dt);
+    void reset();
     void setPidGains(float kp, float ki, float kd);
     void setOutputMax(float newOutputMax);
-    void reset();
     void print() const;
+
+    // Getters
+    float getPidLastOutput() const { return m_pidOutput; }
+    const PidState& getState() const { return m_pidData; }
 
 private:
     float limiter(float value, float min, float max);
