7d2abb8979b1b04b182602a0f7236ed269cc1823
DLon <danlonngren@gmail.com>
2025-07-13 13:36:37 +0100

    Simplify logic and fix gtests

--- simplePIDController/gtests/sPIDControllerTests.cpp
+++ simplePIDController/gtests/sPIDControllerTests.cpp
@@ -3,14 +3,21 @@
 
 #include <gtest/gtest.h>
 
-class CircularBufferTest : public ::testing::Test {
+class SimplePIDControllerTests : public ::testing::Test {
 protected:
     void SetUp() override {
+    }
+    void TearDown() override {
     }
 };
 
 // Test: Buffer Creation
-TEST_F(CircularBufferTest, createPIDController) {
-    sPIDController pid(0.0f, 0.0f, 0.0f, 0.0f);
-    EXPECT_FLOAT_EQ(pid.evaluate(0.0f, 0.0f, 0.0f), 0.0f);
+TEST_F(SimplePIDControllerTests, PIDControllerCreate) {
+    sPIDController pid(1.0f, 1.0f, 1.0f, 1000.0f);
+    EXPECT_FLOAT_EQ(pid.evaluate(5.0f, 10.0f, 0.1f), 5.5f);
+}
+
+TEST_F(SimplePIDControllerTests, PIDControllerLimitTest) {
+    sPIDController pid(1.0f, 1.0f, 1.0f, 100.0f);
+    EXPECT_FLOAT_EQ(pid.evaluate(100.0f, 500.0f, 0.1f), 100.0f);
 }
--- simplePIDController/sPIDController.cpp
+++ simplePIDController/sPIDController.cpp
@@ -1,36 +1,76 @@
 #include "sPIDController.h"
 
-sPIDController::sPIDController(float kp, float kd, float ki, float outMax) :
-    m_pidData(0.0f), m_kp(kp), m_ki(ki), 
-    m_kd(kd), m_maxOutput(outMax), m_pidOutput(0.0f) {}
+#include <iostream>
+
+sPIDController::sPIDController(float kp, float ki, float kd, float outMax) :
+    m_error(0.0f), 
+    m_iError(0.0f), 
+    m_dError(0.0f), 
+    m_eLast(0.0f), 
+    m_kp(kp), 
+    m_ki(ki), 
+    m_kd(kd), 
+    m_maxOutput(outMax), 
+    m_pidOutput(0.0f) {}
 
 float sPIDController::evaluate(float input, float setpoint, float dt) {
-    m_pidData.update(setpoint - input, dt, m_maxOutput);
+    // Avoid division by zero
+    if (dt <= 0.0f) 
+        return m_pidOutput;
+  
+    m_error = setpoint - input;
+    
+    // If we just started, initialize the last error
+    // This prevents a large spike in the first output
+    if (!m_started) {
+        m_started = true;
+        m_eLast = m_error;
+    }
 
-	m_pidOutput = m_pidData.getPidOutput(m_kp, m_kd, m_ki);
-
-    m_pidOutput = limiter(m_pidOutput, -m_maxOutput, m_maxOutput);
-
+    m_dError = (m_error - m_eLast) / dt;
+    m_eLast = m_error;
+    
+    // Integral error with anti-windup
+    m_iError += m_error * dt;
+    if (m_iError > m_maxOutput)
+        m_iError = m_maxOutput;
+    else if (m_iError < -m_maxOutput)
+        m_iError = -m_maxOutput;
+    
+    // PID output calculation
+	m_pidOutput = m_error * m_kp + m_iError * m_ki + m_dError * m_kd;
+    
+    m_pidOutput = limiter(m_pidOutput, (-m_maxOutput), m_maxOutput);
+        
 	return m_pidOutput;
 }
 
 void sPIDController::reset() {
-	m_pidData.clear();
+	m_error = m_dError = m_iError = m_eLast = 0.0f;
     m_pidOutput = 0.0f;
+    m_started = false;
 }
 
-void sPIDController::setPidGains(float kp, float kd, float ki) {
+void sPIDController::setPidGains(float kp, float ki, float kd) {
 	m_kp = kp;
-	m_kd = kd;
 	m_ki = ki;
+	m_kd = kd;
 }
 
 void sPIDController::setOutputMax(float outMax) {
 	m_maxOutput = outMax;
 }
 
-float sPIDController::limiter(float value, float max, float min) {
+float sPIDController::limiter(float value, float min, float max) {
     if (value > max) return max;
     if (value < min) return min;
     return value;
+}
+
+void sPIDController::print() const {
+    std::cout << "PID Output: " << m_pidOutput << "\n"
+              << "Error: " << m_error << "\n"
+              << "Integral Error: " << m_iError << "\n"
+              << "Derivative Error: " << m_dError << "\n"
+              << "Last Error: " << m_eLast << "\n";
 }
